d8387ce1c4fd4715ec698887ce50df749b6119f7
QiuchiChen <30280098+QiuchiChen@users.noreply.github.com>
2017-08-03 02:54:26 +1000

    Pages added
    Newpost page added, logout function added,

--- TheForum/TheForum/InAccountPage.xaml.cs
+++ TheForum/TheForum/InAccountPage.xaml.cs
@@ -10,6 +10,74 @@ namespace TheForum
         public InAccountPage()
         {
             InitializeComponent();
-        }
+            BackgroundColor = Color.Black;
+           
+            var table = new TableView
+            {
+
+                BackgroundColor = Color.White,
+                Intent = TableIntent.Form,
+
+
+                Root = new TableRoot("Table Title") {
+                    new TableSection ("User&title") {
+                        
+                        new TextCell {
+                            Detail = "Name",
+                            Text = "admin",
+
+                            TextColor = Color.NavajoWhite
+						},
+						new EntryCell {
+							Label = "Title:",
+							Placeholder = "Enter the title here",
+							Keyboard = Keyboard.Default
+						}
+					},
+					new TableSection ("Main Body") {
+						new EntryCell {
+							Label = "Body:",
+							Placeholder = "Enter the body here",
+							Keyboard = Keyboard.Telephone
+						},
+						
+					}
+				}
+				
+
+			};
+			Button PostButton = new Button
+			{
+				Text = "Post",
+				TextColor = Color.White,
+                BackgroundColor = Color.Silver,
+				FontSize = 20,
+				FontAttributes = FontAttributes.Bold,
+				HorizontalOptions = LayoutOptions.FillAndExpand
+			};
+			PostButton.Clicked += newpost;
+
+			async void newpost(object sender, EventArgs e)
+			{
+				await Navigation.PushAsync(new NavigationPage(new TabbedPage()
+
+				{
+					Children = {
+					 new TheForumPage (),
+                     //new LoginPage() ,
+                   
+                    new settings(),
+
+
+				}
+				}));
+			}
+			Content = new StackLayout
+			{
+				Padding = 30,
+				Spacing = 30,
+                Children = { table, PostButton }
+			};
+		}
     }
 }
--- TheForum/TheForum/TheForumPage.xaml.cs
+++ TheForum/TheForum/TheForumPage.xaml.cs
@@ -44,20 +44,86 @@ namespace TheForum
 
 
 
-			var tableview = new TableView
+			 var tableview = new TableView
+			 {
+
+
+
+				 Root = new TableRoot(){
+					    new TableSection("Zelda"){
+                        
+						 new TextCell {
+                           
+							 Text = "The Legend of Zelda!!!!",
+							 Detail ="The legend which brings ur dream alive!",
+						 },
+                       
+
+						 new TextCell {
+							 ClassId = "2",
+							 Text = "Zelda best game of the year",
+							 Detail ="80% of the switch user have got Zelda! ",
+
+						 },
+
+
+						 new TextCell {
+							 Text = "Zelda New meta!!",
+							 Detail ="WOW its cooooooooooooool",
+						 }
+					 }}
+			 };
+			
+			/*var tableview = new TableView();
+			tableview.Intent = TableIntent.Settings;
+			
+         
+			
+            var layout = new StackLayout() { Orientation = StackOrientation.Horizontal };
+
+        
+            layout.Children.Add(new Button()
 			{
-				Root = new TableRoot(){
-					new TableSection("Zelda"){
-						new TextCell {
-							Text = "The Legend of Zelda",
-							Detail ="WOW its cooooooooooooool",
-						}
+                Image = "lol.png",
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.EndAndExpand
+			});
+
+			
+
+			var layout1 = new StackLayout() { Orientation = StackOrientation.Horizontal };
+
+
+			layout1.Children.Add(new Button()
+			{
+				Image = "car.png",
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.CenterAndExpand                               
+			});
+
+
+
+
+			tableview.Root = new TableRoot()
+            {
+                new TableSection("Games")
+                {
+                new ViewCell() {View = layout}
+                },
+
+				new TableSection("Car")
+				{
+				new ViewCell() {View = layout1}
 				}
 
-				}
-			};
+			
+            };*/
 
 
+			
+
+			
+			
 
 			Content = new StackLayout
 			{
