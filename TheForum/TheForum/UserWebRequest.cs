ea92935b5759d08cff2de97bc2836462bc9e68fb
QiuchiChen <30280098+QiuchiChen@users.noreply.github.com>
2017-09-18 22:16:48 +1000

    loading screen

--- TheForum/TheForum/UserWebRequest.cs
+++ TheForum/TheForum/UserWebRequest.cs
@@ -48,14 +48,14 @@ namespace TheForum
 
 	
         //security SHA
-		public string SHA256Hash(string input)
+		/*public string SHA256Hash(string input)
 		{
 			byte[] sign_byte = Encoding.UTF8.GetBytes(input);
 			var sha2 = SHA256.Create();
 			sign_byte = sha2.ComputeHash(sign_byte);
 			return Encoding.UTF8.GetString(sign_byte);
 		}
-     
+     */
 		public async void Save()
 		{
 			try
